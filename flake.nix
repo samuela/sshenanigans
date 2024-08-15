c4e418f37c3f7ca7026f0add3f842c4aab63c80b
Samuel Ainsworth <skainsworth@gmail.com>
2024-08-15 00:22:33 +0000

    russh: 0.43.0 -> 0.45.0, remove OpenSSL dependency

--- flake.nix
+++ flake.nix
@@ -9,36 +9,21 @@
       let
         pkgs = nixpkgs.legacyPackages."${system}";
         naersk-lib = naersk.lib."${system}";
-      in
-      with pkgs;
-      rec {
+      in rec {
         # `nix build`
         packages.default = naersk-lib.buildPackage {
           pname = "sshenanigans";
           root = ./.;
-
-          # See https://github.com/nix-community/naersk?tab=readme-ov-file#using-openssl.
-          nativeBuildInputs = [ pkg-config ];
-          buildInputs = [ openssl.dev ];
         };
 
         # `nix run`
-        apps.default = utils.lib.mkApp {
-          drv = packages.default;
-        };
+        apps.default = utils.lib.mkApp { drv = packages.default; };
 
         # `nix develop`
-        devShell = mkShell {
-          nativeBuildInputs = ([
-            rustc
-            cargo
-            rustfmt
-
-            # See https://nixos.wiki/wiki/Rust#Building_Rust_crates_that_require_external_system_libraries.
-            # sshenanigans uses russh with the "openssl" feature, which requires this.
-            openssl.dev
-            pkg-config
-          ] ++ lib.optionals stdenv.isDarwin [ libiconv ]);
-        };
+        devShell = with pkgs;
+          mkShell {
+            nativeBuildInputs = ([ cargo rustc rustfmt ]
+              ++ lib.optionals stdenv.isDarwin [ libiconv ]);
+          };
       });
 }
